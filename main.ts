5ba8bf7e08e0de5de094d808fee89a6e3773b928
Abdullah AlHamdan <abdallahatrash@gmail.com>
2023-05-31 14:52:19 +0200

    Create main.ts

---
+++ main.ts
@@ -0,0 +1,3 @@
+import { dirname } from 'https://deno.land/x/dirname/mod.ts'
+import { sh } from 'https://deno.land/x/drake@v0.41.0/mod.ts'
+import run from 'https://deno.land/x/test_on_localhost@0.2.2/run.ts'
