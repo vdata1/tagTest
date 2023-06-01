3828bc4d94d69dfa19cf1d910cea5745b0c54148
Abdullah AlHamdan <abdallahatrash@gmail.com>
2023-06-01 10:07:20 +0200

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,3 +1,3 @@
 import { assertEquals } from "https://deno.land/std@0.138.0/testing/asserts.ts";
 import { readableStreamFromIterable } from "https://deno.land/std@0.138.0/streams/conversion.ts";
-import fc from "https://cdn.skypack.dev/fast-check";
+
