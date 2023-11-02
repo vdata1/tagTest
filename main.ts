265beff7053faf181658f4c6190ff216ae51d0de
c01abal <abdullah.alhamdan@cispa.de>
2023-11-02 16:12:03 +0100

    change 5

--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { dirname } from 'https://deno.land/x/dirname/mod.ts'
 import { sh } from 'https://deno.land/x/drake@v0.41.0/mod.ts'
 import run from 'https://deno.land/x/test_on_localhost@0.2.2/run.ts'
-
+var A = "change4";
 var w = "chage3"
 var s = "chage2"; 
 var x= "Changes";
